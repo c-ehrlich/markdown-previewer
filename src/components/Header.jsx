55bd41b45b4efe93aede4996c94d04cf8d7b48e4
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-11-19 16:16:53 +0100

    use icons to show light/dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,22 @@ import * as React from "react";
 
 import {
   AppBar,
-  FormControlLabel,
+  IconButton,
   Toolbar,
   Typography,
   Switch,
 } from "@mui/material";
 
+import yellow from "@material-ui/core/colors/yellow";
+import blueGrey from "@material-ui/core/colors/blueGrey";
+
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+
+import Brightness2Icon from "@mui/icons-material/Brightness2";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMarkdown } from "@fortawesome/free-brands-svg-icons";
@@ -23,23 +29,21 @@ export default function ButtonAppBar() {
   const setDark = useStore((state) => state.setDark);
 
   return (
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          <FontAwesomeIcon icon={faMarkdown} />  Markdown Previewer
-          </Typography>
-          <FormControlLabel
-            control={
-              <Switch
-                checked={dark}
-                onChange={() => setDark(!dark)}
-                name="gilad"
-              />
-            }
-            labelPlacement="start"
-            label={ dark ? "Dark Mode" : "Light Mode" }
-          />
-        </Toolbar>
-      </AppBar>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <FontAwesomeIcon icon={faMarkdown} /> Markdown Previewer
+        </Typography>
+        <IconButton area-label="light-mode" onClick={() => setDark(false)}>
+          <WbSunnyIcon style={{ color: dark ? blueGrey[500] : yellow[200] }} />
+        </IconButton>
+
+        <Switch checked={dark} onChange={() => setDark(!dark)} name="gilad" />
+        <IconButton area-label="dark-mode" onClick={() => setDark(true)}>
+          <Brightness2Icon />
+        </IconButton>
+        {/* </Stack> */}
+      </Toolbar>
+    </AppBar>
   );
 }
